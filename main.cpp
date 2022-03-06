2b3bf2b6f6fc70eb72038792ed3ce46862e91d88
E.Demin <tlmiracle@yandex.ru>
2022-03-06 17:11:06 +0400

    Создал рекурсивную функцию для вычисления числа Фибоначчи

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
-int main(){
-cout << "Hello, world!";
+long fib(int n) {
+	if (n <= 0) return -1;
+	if (n == 1 || n == 2) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+int main() {
+	cout << fib(10);
 }
